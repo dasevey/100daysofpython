acff82f7b47024417f61e0be091d68b92d452686
dasevey <sevey.adam@gmail.com>
2022-12-13 12:11:41 -0700

    Day1

--- day1.py
+++ day1.py
@@ -3,4 +3,40 @@
 # # exercise 1
 # print(f"Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
 
-# exercise 2
+# # exercise 2
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# # exercise 3
+# name = input("What is your name? ")
+# print(len(name))
+
+# # exercise 4
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# switch = a
+# a = b
+# b = switch
+
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+# Final exercise
+print("Welcome to the Band Name Generator.")
+city = input(f"What city do you grow up in?\n")
+pet = input(f"What's the name of your pet?\n")
+
+print(f"Your band name could be {city} {pet}")
